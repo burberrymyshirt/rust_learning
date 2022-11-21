5f7e6467f6864a9281ed37437f67f1c319cc8ce8
burberrymyshirt <117170518+burberrymyshirt@users.noreply.github.com>
2022-11-21 21:55:41 +0100

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-4364d943b0c27e77.d
+++
@@ -1,5 +0,0 @@
-c:\Users\lol29\Documents\rust_workspace\rust_learning\hello_cargo\target\debug\deps\hello_cargo-4364d943b0c27e77.rmeta: src\main.rs
-
-c:\Users\lol29\Documents\rust_workspace\rust_learning\hello_cargo\target\debug\deps\hello_cargo-4364d943b0c27e77.d: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/debug/deps/hello_cargo-9a380041da3a0e62.d
+++
@@ -1,5 +0,0 @@
-c:\Users\lol29\Documents\rust_workspace\rust_learning\hello_cargo\target\debug\deps\hello_cargo-9a380041da3a0e62.rmeta: src\main.rs
-
-c:\Users\lol29\Documents\rust_workspace\rust_learning\hello_cargo\target\debug\deps\hello_cargo-9a380041da3a0e62.d: src\main.rs
-
-src\main.rs:
