36fbdb6e280708107bae4fe766eb05d09359cb16
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2022-11-24 14:31:10 +0100

    changed commenting style on variables and filled out branches

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-/*     let mut x = 5; 
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}"); */
+    // let mut x = 5; 
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
 
 
     let _x = 5;
