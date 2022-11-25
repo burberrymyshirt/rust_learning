9d188df9b52f956a9aa249efa9403be600f4b38c
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2022-11-25 16:31:37 +0100

    converted from spaces to tabs

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let number = 8;
+	let number = 8;
 
-    if number % 4 == 0 {
-        println!("this number is divisable by 4");
-    } else if number % 3 == 0 {
-        println!("this number is divisable by 3");
-    } else if number % 2 == 0 {
-        println!("this number is divisable by 2");
-    } else {
-        println!("this number is neither divisable by 4, 3 or 2")
-    }
+	if number % 4 == 0 {
+		println!("this number is divisable by 4");
+	} else if number % 3 == 0 {
+		println!("this number is divisable by 3");
+	} else if number % 2 == 0 {
+		println!("this number is divisable by 2");
+	} else {
+		println!("this number is neither divisable by 4, 3 or 2")
+	}
 }
 //
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    // let mut x = 5; 
-    // println!("The value of x is: {x}");
-    // x = 6;
-    // println!("The value of x is: {x}");
+	// let mut x = 5; 
+	// println!("The value of x is: {x}");
+	// x = 6;
+	// println!("The value of x is: {x}");
 
 
-    let _x = 5;
+	let _x = 5;
 
-    let x = 5 + 1;
+	let x = 5 + 1;
 
-    {
-        let x = x * 2;
-        println!("The x value of the inner scope is: {x}");
-    }
+	{
+		let x = x * 2;
+		println!("The x value of the inner scope is: {x}");
+	}
 
-    println!("The x value is: {x}");
+	println!("The x value is: {x}");
 }
