54e89b796702c1e1be728004a107f4cf10060854
burberrymyshirt <magnushlarsen01@gmail.com>
2022-12-01 12:03:51 +0100

    added some code :)

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This program will convert between Fahrenheit and Celsius");
+
+    println!("Would you like to convert from Celsius(1) or from Fahrenheit(2)");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read line");
+
+
+    
+    if input == 1 && goon {
+        celcius();
+    }
 }
