e65f024cd0dda7712a489d46e89c1a002a07d3a5
burberrymyshirt <magnushlarsen01@gmail.com>
2022-12-02 11:23:30 +0100

    made proggress in temp_converter

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -3,17 +3,50 @@ use std::io;
 fn main() {
     println!("This program will convert between Fahrenheit and Celsius");
 
-    println!("Would you like to convert from Celsius(1) or from Fahrenheit(2)");
+    loop {
+        println!("Would you like to convert from Celsius(1) or from Fahrenheit(2)");
 
-    let mut input = String::new();
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
 
-
-    
-    if input == 1 && goon {
-        celcius();
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        if input == 1 {
+            celcius();
+        } else if input == 2 {
+            fahrenheit();
+        }
     }
+}
+
+
+fn celcius() {
+    loop {
+        print!("Input your degrees in celcius: ");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let input :f32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        
+    }
+
+}
+
+fn fahrenheit() {
+
+
 }
