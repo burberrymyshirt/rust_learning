abb22cffa4ab1e857e14e0139547c298ec5f7c0f
burberrymyshirt <magnushlarsen01@gmail.com>
2022-12-05 09:55:45 +0100

    created twelve_days_of_christmas projekt, and started work

---
+++ twelve_days_of_christmas/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+	
+	let a = ["A partridge in a pear tree", "Two turtle doves, and", 
+		"Three french hens", "Four calling birds", "Five golden rings", 
+		"Six geese a-laying", "Seven swans a-swimming", "Eight maids a-milking", 
+		"Nine ladies dancing", "Ten lords a-leaping", "Eleven pipers piping", 
+		"Twelve drummers drumming"] ;
+
+	let b = ["first", "second", "third", "fourth", "fifth", "sixth", 
+		"seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+
+		for number in b {
+			println!("On the {number} day of christmas,");
+			println!("my true love gave to me");
+
+
+
+		}
+
+}
