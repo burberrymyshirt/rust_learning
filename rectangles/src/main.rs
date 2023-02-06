5a06368dfaa05350907634a62cf4e86a31c0cc99
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-02-06 08:21:18 +0100

    finished rectangles project

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -30,22 +30,44 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        return self.width > other.width && self.height > other.height;
+    }
+
+    fn square(size: u32) -> Self {
+        return Self {
+            width: size, 
+            height: size,
+        };
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60, 
+        height: 45,
+    };
+
+    let square = Rectangle::square(32);
+
     println!("rect1 is {} high and {} wide", rect1.height, rect1.width);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        return self.width * self.height;
-    }
-}
