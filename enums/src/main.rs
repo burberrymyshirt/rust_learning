9fccf632c3ec47bfadbd2fad14006e3acefec22c
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-02-17 18:35:58 +0100

    finished enums and enums2 learning projects

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
     println!("Hello, world!");
     
-let loopback = IpAddr::V6(String::from("::1"));
-let home = IpAddr::V4(127, 0, 0, 1);
-
-fn route (ip_kind: IpAddr) {}
+let _loopback = IpAddrKind::V6(String::from("::1"));
+let _home = IpAddrKind::V4(127, 0, 0, 1);
 }
 
-enum IpAddr {
+enum IpAddrKind {
     V4(u8, u8, u8, u8), 
     V6(String), 
 }
 
-
+fn _route (_ip_kind: IpAddrKind) {}
--- enums2/src/main.rs
+++ enums2/src/main.rs
@@ -1,12 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let m = Message::Write(String::from("Hellooo"));
+    m.call(); 
 }
 
+enum Message {
+    Quit, 
+    Move { x:i32, y: i32},
+    Write(String),
+    ChangeColor( i32, i32, i32),
+}
 
-
-
-
-
+impl Message {
+    fn call (&self) {
+        // empty function for demonstration purposes
+    }
+}
 
 
 
