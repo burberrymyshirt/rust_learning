e9fceb2f987756260277888af3265a9608c0f0f7
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-02-17 19:01:23 +0100

    fixed a dumbass mistake that wasn't a mistake at all

--- enums2/src/main.rs
+++ enums2/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
 }
 
 enum Message {
-	Quit, 
-	Move { x:i32, y: i32},
+	_Quit, 
+	_Move { x:i32, y: i32},
 	Write(String),
-	ChangeColor( i32, i32, i32),
+	_ChangeColor( i32, i32, i32),
 }
 
 impl Message {
