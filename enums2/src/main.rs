a5c304e4f2e8dacdd5b54f8249fbb5c6b07b562f
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-02-17 18:37:36 +0100

    converted spaces to tabs

--- enums2/src/main.rs
+++ enums2/src/main.rs
@@ -1,19 +1,19 @@
 fn main() {
-    let m = Message::Write(String::from("Hellooo"));
-    m.call(); 
+	let m = Message::Write(String::from("Hellooo"));
+	m.call(); 
 }
 
 enum Message {
-    Quit, 
-    Move { x:i32, y: i32},
-    Write(String),
-    ChangeColor( i32, i32, i32),
+	Quit, 
+	Move { x:i32, y: i32},
+	Write(String),
+	ChangeColor( i32, i32, i32),
 }
 
 impl Message {
-    fn call (&self) {
-        // empty function for demonstration purposes
-    }
+	fn call (&self) {
+		// empty function for demonstration purposes
+	}
 }
 
 
