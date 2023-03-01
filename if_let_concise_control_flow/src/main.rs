caaaf98a7224f37fb39b5412d2369f61cd13fa90
Magnus H. Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-03-01 06:01:42 +0100

    added a comment to if let controlflow project

--- if_let_concise_control_flow/src/main.rs
+++ if_let_concise_control_flow/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    //println!("Hello, world!");
-	
+
+	// These two blocks of code do the exact same thing, where the only difference is, that the if let statement is more concise and short.
+	// It lets you omit the "_" in the match statement, which is less boilerplate
 	let config_max = Some(3u8);
 
 	//match config_max {
@@ -11,7 +12,7 @@ fn main() {
 	if let Some(max) = config_max {
 		println!("The maximum is configured to be {}", max);
 	}
-	// you can include an else statement after the if let, corrosponding to
+	// you can include an else statement after the if let, corresponding to
 	// "_" in the match statement.
 
 }
