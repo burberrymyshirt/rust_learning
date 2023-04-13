1d2a5d487b6005408d7d3063b4a25a34a2a52fec
Magnus Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-04-13 10:16:46 +0200

    more on hashmap project

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -14,4 +14,44 @@ fn main() {
     for (key, value) in &scores {
         println!("{key}: {value}");
     }
+
+    //where using an i32 that implements the copy trait will not take ownership,
+    //Strings are different in that they do not implement the copy trait, and a HashMap will therefore take ownership.
+    //finally, if a reference to a string is passed, the reference itself must be valid for at least as long as the map.
+    let field_name = String::from("Favourite color");
+    let field_value = String::from("Yellow");
+
+    let mut map = HashMap::new();
+
+    map.insert(field_name, field_value);
+
+    //overwriting a value
+    let mut scores2 = HashMap::new();
+
+    scores2.insert(String::from("Blue"), 10);
+    //The second insert will overwrite the first, as the same key can't have two values.
+    scores2.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores2);
+
+    //inserting a key,value pair, only if a key doesn't already exist
+    let mut scores3 = HashMap::new();
+    scores3.insert(String::from("Blue"), 10);
+
+    scores3.entry(String::from("Yellow")).or_insert(50);
+    scores3.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores3);
+
+    //this will update a value based on if the old value
+    let text = String::from("hello world wonderful world");
+
+    let mut map2 = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map2.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map2);
 }
