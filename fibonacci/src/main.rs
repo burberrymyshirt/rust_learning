8c394bf4411d81ceb2736e081e91254e8a3803de
Magnus Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-05-10 10:08:44 +0200

    optimized fib

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,43 +1,38 @@
 use std::io;
 
 fn main() {
-	loop {
-		println!("which fibo number do you wanna calculate? O.O");
+    loop {
+        println!("which fibo number do you wanna calculate? O.O");
 
-		let mut input = String::new();
+        let mut input = String::new();
 
-		io::stdin()
-			.read_line(&mut input)
-			.expect("de her");
+        io::stdin().read_line(&mut input).expect("de her");
 
-		let input: u128 = match input.trim().parse() {
-			Ok(num) => num, 
-			Err(_) => continue,
-		};
+        let input: u8 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-		let fib_num = fib(input);
+        let fib_num = fib(input);
 
-		println!("{fib_num}");
-	}
+        println!("{fib_num}");
+    }
 }
 
-fn fib(n: u128) -> u128 {
+fn fib(n: u8) -> u128 {
+    let mut n1 = 1;
+    let mut n2 = 1;
+    let mut n3 = 0;
 
-	let mut n1 = 1;
-	let mut n2 = 1; 
-	let mut n3 = 0;
-	
-	if n < 3 {
-		n3 = 1;
-	} else {
-		for _ in 3..n {
-			n3 = n1 + n2;
-			n1 = n2;
-			n2 = n3;
-		}
-	}
+    if n < 3 {
+        n3 = 1;
+    } else {
+        for _ in 3..n {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+    }
 
-	return n3;
+    return n3;
 }
-
-
