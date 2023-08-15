87992d48a515038661db6de9ee9925e69958e64e
Magnus Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-08-15 12:49:42 +0200

    created new chapter 8 exercise project

---
+++ chap8exercise3/src/main.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    let employees = vec!["Joanne", "Tanja", "goated", "sauce"];
+    let departments: Vec<&str> = vec!["Sales", "deez", "nutz"];
+    let mut working_in: HashMap<String, String> = HashMap::new();
+}
