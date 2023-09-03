d0cbc0502a17f4b1cf8791267550003e7c03c7d2
Magnus Larsen <117170518+burberrymyshirt@users.noreply.github.com>
2023-09-03 21:21:17 +0200

    chap8exercise 3 work

--- chap8exercise3/src/main.rs
+++ chap8exercise3/src/main.rs
@@ -2,6 +2,10 @@ use std::collections::HashMap;
 use std::io;
 
 fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
     let employees = vec!["Joanne", "Tanja", "goated", "sauce"];
     let departments: Vec<&str> = vec!["Sales", "deez", "nutz"];
     let mut working_in: HashMap<String, String> = HashMap::new();
